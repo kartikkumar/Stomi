6223856c798aead88fabc1b5b0eb9683e1f4bf25
Kartik Kumar <me@kartikkumar.com>
2013-07-06 00:00:25 +0200

    Updated license references in file headers. Added test particle input files and added caseId variable.

---
+++ StochasticMigration/Database/testParticleInput.cpp
@@ -0,0 +1,125 @@
+/*    
+ *    Copyright (c) 2010-2013, Delft University of Technology
+ *    All rights reserved.
+ *    See COPYING for license details.
+ *
+ *    Changelog
+ *      YYMMDD    Author            Comment
+ *      130218    K. Kumar          File created.
+ *      130309    K. Kumar          Moved constructor implementation to source file and added
+ *                                  sanity checks to ensure that test particle kick is valid; Moved
+ *                                  all existing operator overloads to non-member functions and
+ *                                  added new ones, including for pointer comparisons.
+ *      130328    K. Kumar          Moved standard operator overload functions to Assist; used  
+ *                                  comparison functions in Assist for checks in constructor.
+ *      130705    K. Kumar          Added caseId variable.
+ *
+ *    References
+ *
+ *    Notes
+ *
+ */
+
+#include <TudatCore/Astrodynamics/BasicAstrodynamics/orbitalElementConversions.h>
+#include <TudatCore/Astrodynamics/BasicAstrodynamics/unitConversions.h>
+
+#include <Assist/Basics/comparisonFunctions.h>
+
+#include "StochasticMigration/Database/testParticleInput.h"
+
+namespace stochastic_migration
+{
+namespace database
+{
+
+using namespace assist::basics;
+using namespace tudat::basic_astrodynamics::orbital_element_conversions;
+
+//! Default constructor, initializing class members.
+TestParticleInput::TestParticleInput(
+        const int aSimulationNumber,
+        const int aCaseId,
+        const bool aCompletedFlag,
+        const tudat::basic_mathematics::Vector6d& anInitialStateInKeplerianElements )
+    : simulationNumber( checkPositive( aSimulationNumber, "Simulation number" ) ),
+      caseId( checkPositive( aCaseId, "Case id" ) ),
+      isCompleted( aCompletedFlag ),
+      initialStateInKeplerianElements( 
+        ( Eigen::VectorXd( 6 ) 
+            << checkPositive( anInitialStateInKeplerianElements( semiMajorAxisIndex ),
+                              "Initial semi-major axis [m]" ),
+               checkInRange( anInitialStateInKeplerianElements( eccentricityIndex ), 
+                             "Initial eccentricity [-]", 0.0, 1.0 ),
+               checkPositive( anInitialStateInKeplerianElements( inclinationIndex ),
+                              "Initial inclination [rad]" ),
+               anInitialStateInKeplerianElements.segment( 3, 3 ) ).finished( ) )
+{ }
+
+//! Overload == operator.
+bool operator==( const TestParticleInput& testParticleInput1,
+                 const TestParticleInput& testParticleInput2 )
+{
+    return ( testParticleInput1.simulationNumber == testParticleInput2.simulationNumber
+             && testParticleInput1.caseId == testParticleInput2.caseId
+             && testParticleInput1.isCompleted == testParticleInput2.isCompleted
+             && testParticleInput1.initialStateInKeplerianElements
+             == testParticleInput2.initialStateInKeplerianElements );
+}
+
+//! Overload < operator.
+bool operator<( const TestParticleInput& testParticleInput1,
+                const TestParticleInput& testParticleInput2 )
+{
+    return testParticleInput1.simulationNumber < testParticleInput2.simulationNumber;
+}
+
+//! Overload << operator.
+std::ostream& operator<<( std::ostream& outputStream, const TestParticleInput& testParticleInput )
+{
+    using namespace tudat::basic_astrodynamics::unit_conversions;
+
+    // Set string indicating whether test particle simulation has been completed or not.
+    std::string completedStatus;
+
+    if ( testParticleInput.isCompleted == true )
+    {
+        completedStatus = "TRUE";
+    }
+
+    else
+    {
+        completedStatus = "FALSE";
+    }
+
+    // Write contents of TestParticleInput object to output stream.
+    outputStream << "Test particle simulation number: "
+                 << testParticleInput.simulationNumber << std::endl;
+    outputStream << "Case ID: " << testParticleInput.caseId << std::endl;
+    outputStream << "Test particle simulation completed?: " << completedStatus << std::endl;
+    outputStream << "Test particle's initial semi-major axis [m]: "
+                 << testParticleInput.initialStateInKeplerianElements( semiMajorAxisIndex )
+                 << std::endl;
+    outputStream << "Test particle's initial eccentricity [-]: "
+                 << testParticleInput.initialStateInKeplerianElements( eccentricityIndex )
+                 << std::endl;
+    outputStream << "Test particle's initial inclination [deg]: "
+                 << convertRadiansToDegrees(
+                        testParticleInput.initialStateInKeplerianElements( inclinationIndex ) )
+                 << std::endl;
+    outputStream << "Test particle's initial argument of periapsis [deg]: "
+                 << convertRadiansToDegrees( testParticleInput.initialStateInKeplerianElements(
+                                                 argumentOfPeriapsisIndex ) ) << std::endl;
+    outputStream << "Test particle's initial longitude of ascending node [deg]: "
+                 << convertRadiansToDegrees( testParticleInput.initialStateInKeplerianElements(
+                                                 longitudeOfAscendingNodeIndex ) ) << std::endl;
+    outputStream << "Test particle's initial true anomaly [deg]: "
+                 << convertRadiansToDegrees(
+                        testParticleInput.initialStateInKeplerianElements( trueAnomalyIndex ) )
+                 << std::endl;
+
+    // Return output stream.
+    return outputStream;
+}
+
+} // namespace database
+} // namespace stochastic_migration
---
+++ StochasticMigration/Database/testParticleInput.h
@@ -0,0 +1,106 @@
+/*    
+ *    Copyright (c) 2010-2013, Delft University of Technology
+ *    All rights reserved.
+ *    See COPYING for license details.
+ *
+ *    Changelog
+ *      YYMMDD    Author            Comment
+ *      120402    K. Kumar          File created from old caseSimulationDataRow.h.
+ *      120515    K. Kumar          Renamed file to rowData.h; renamed struct to rowData.
+ *      130212    K. Kumar          Added Doxygen comments and a note. Added planetary_rings
+ *                                  namespace.
+ *      130217    K. Kumar          Updated "mab simulations" references to "stochastic migration";
+ *                                  renamed file.
+ *      130309    K. Kumar          Moved all existing operator overloads to non-member functions
+ *                                  and added new ones, including for pointer comparisons; added
+ *                                  shared-pointer definition.
+ *      130328    K. Kumar          Moved standard operator overload functions to Assist.
+ *      130329    K. Kumar          Updated typedef for test particle kick table to boost ptr_set.
+ *      130705    K. Kumar          Added caseId variable.
+ *
+ *    References
+ *      sbi. C++ Operator Overloading, Stack Overflow,
+ *          http://stackoverflow.com/questions/4421706/operator-overloading, 2010, last accessed:
+ *          9th March, 2013.
+ *
+ *    Notes
+ *
+ */
+
+#ifndef STOCHASTIC_MIGRATION_TEST_PARTICLE_INPUT_H
+#define STOCHASTIC_MIGRATION_TEST_PARTICLE_INPUT_H
+
+#include <iostream>
+#include <limits>
+#include <set>
+
+#include <boost/ptr_container/ptr_set.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include <Eigen/Core>
+
+#include <Tudat/Mathematics/BasicMathematics/linearAlgebraTypes.h>
+
+namespace stochastic_migration
+{
+namespace database
+{
+
+//! Data struct that contains input data for one test particle simulation.
+/*!
+ * This data struct contains input data, stored in an SQLite3 database. The data stored is used to
+ * set up a test particle simulation.
+ */
+struct TestParticleInput
+{
+public:
+
+    // Set Eigen macro to correctly align class with fixed-size vectorizable types.
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    //! Default constructor, initializing class members.
+    TestParticleInput(
+            const int aSimulationNumber,
+            const int aCaseId,
+            const bool aCompletedFlag,
+            const tudat::basic_mathematics::Vector6d& anInitialStateInKeplerianElements );
+
+    //! Simulation number.
+    const int simulationNumber;
+
+    //! Case ID.
+    const int caseId;
+
+    //! Flag indicating if simulation has been completed/executed already.
+    const bool isCompleted;
+
+    //! Initial state of test particle in Keplerian elements.
+    const tudat::basic_mathematics::Vector6d initialStateInKeplerianElements;
+
+protected:
+private:
+};
+
+//! Typedef for shared-pointer to TestParticleInput object.
+typedef boost::shared_ptr< TestParticleInput > TestParticleInputPointer;
+
+//! Typedef for input table (pointers) for test particle simulations.
+typedef boost::ptr_set< TestParticleInput > TestParticleInputTable;
+
+// Define all of the operator overloads as non-member functions (sbi, 2010).
+
+//! Overload == operator.
+bool operator==( const TestParticleInput& testParticleInput1,
+                 const TestParticleInput& testParticleInput2 );
+
+//! Overload < operator.
+bool operator<( const TestParticleInput& testParticleInput1,
+                const TestParticleInput& testParticleInput2 );
+
+//! Overload << operator.
+std::ostream& operator<<( std::ostream& outputStream, const TestParticleInput& testParticleInput );
+
+} // namespace database
+} // namespace stochastic_migration
+
+#endif // STOCHASTIC_MIGRATION_TEST_PARTICLE_INPUT_H
