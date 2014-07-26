a7e3de01d009e07b25aa70e727951a2daf09d827
Kartik Kumar <me@kartikkumar.com>
2014-07-26 15:26:33 +0200

    Update script to populate database with random walk runs and change file permissions. Update names for random walk generator config files.

--- Config/Scripts/populateRandomWalkRuns.sh
+++ Config/Scripts/populateRandomWalkRuns.sh
@@ -3,23 +3,16 @@
 # Set environment variables.
 stomi="/home/kartik/app/stomi/bin/stomi"
 
-# Declare array of strings with variants for random walk database generator configuration files.
-declare -a randomWalkDatabaseGeneratorVariants=("nominal" "light" "heavy" "sparse" "dense")
-
 # Set template command to populate database with random walk runs.
-declare -r randomWalkTemplateCommand="$stomi ../RandomWalkDatabaseGenerator/RUN_VARIANT_randomWalkDatabaseGeneratorSettings.cfg"
+declare -r randomWalkTemplateCommand="$stomi ../RandomWalkDatabaseGenerator/runID_randomWalkDatabaseGeneratorSettings.cfg"
 
 # Loop through randoom walk runs and populate database.
-for run in {1..15}
+for runId in {1..15}
 do
-    for variant in "${randomWalkDatabaseGeneratorVariants[@]}" 
-    do
-        # Execute command to populate database.
-        temporaryCommand="${randomWalkTemplateCommand/RUN/$run}"
-        command="${temporaryCommand/VARIANT/$variant}"
-        $command
+    # Execute command to populate database.
+    command="${randomWalkTemplateCommand/ID/$runId}"
+    echo  $command
 
-        # Wait for previous process to end before proceeding.
-        wait
-    done
+    # Wait for previous process to end before proceeding.
+    wait
 done
